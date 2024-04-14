cdce945acf529381db2b4989d139bd6e2c53f8ca
sanjanachecker <sanjanachecker@gmail.com>
2024-04-14 12:41:04 -0700

    find user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,10 +33,24 @@ const users = {
     ]
   };
 
+  const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+
 app.use(express.json());
 
 app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
     res.send(users);
+  }
 });
 
 app.listen(port, () => {
