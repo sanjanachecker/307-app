ca5dc4f6e4d1f4c16c9e7b3989d13c4fa30e9dfb
sanjanachecker <sanjanachecker@gmail.com>
2024-04-17 11:02:03 -0700

    connected front and backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,16 @@
 // src/MyApp.jsx
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Table from "./Table"
 import Form from "./Form"
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchUsers()
+    .then((res) => res.json())
+    .then((json) => setCharacters(json["users_list"]))
+    .catch((error) => { console.log(error); });
+  }, [] );
   function removeOneCharacter(index){
     const updated = characters.filter((character, i) => {
       return i !== index
@@ -13,6 +19,10 @@ function MyApp() {
   }
   function updateList(person) {
     setCharacters([...characters, person]);
+  }
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
   }
   return (
     <div className = "container">
